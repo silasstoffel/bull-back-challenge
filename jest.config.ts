3c30c4fe6d0172d817780216c324cdb490f1a70f
Silas Stoffel <silasstofel@gmail.com>
2023-03-10 20:19:24 -0300

    test(e2e): adding load account test

--- jest.config.ts
+++ jest.config.ts
@@ -12,7 +12,6 @@ const config: Config = {
     "<rootDir>/jest.setup.ts"
   ],
   detectOpenHandles: false,
-  collectCoverage: true,
   coveragePathIgnorePatterns: [
     "node_modules",
     "@types",
---
+++ tests/e2e/modules/accounts/infra/http/load-account-controller.spec.ts
@@ -0,0 +1,40 @@
+import { initializeDataSource } from "../../../../../helpers/setup-data-source";
+import request from 'supertest';
+import { app } from "../../../../../../src/shared/infra/http/app";
+
+describe("LoadAccountController", () => {
+
+    let bearer: string;
+    const name = "Account 3";
+    const id = "6bc53670-8747-4245-bc6f-458cf735f476";
+
+    beforeAll(async () => {
+        await initializeDataSource();
+
+        const data = await request(app)
+        .post('/auth')
+        .send({
+            email: "account3@bull.com.br",
+            password: "123456"
+        }).expect(200);
+
+        bearer = `Bearer ${data.body.token}`
+    });
+
+    describe("when the credentials are correctly", () => {
+        it('should return account info', async() => {
+            const response = await request(app).get('/auth/me').set({ Authorization: bearer });
+            expect(response.status).toBe(200);
+            expect(response.body).toHaveProperty('id', id);
+            expect(response.body).toHaveProperty('name', name);
+        });
+    });
+
+    describe("when the bearer property is not valid or expired", () => {
+        it('should return account info', async() => {
+            bearer = "Bearer invalid-value"
+            await request(app).get('/auth/me').set({ Authorization: bearer }).expect(401);
+        });
+    });
+
+});
