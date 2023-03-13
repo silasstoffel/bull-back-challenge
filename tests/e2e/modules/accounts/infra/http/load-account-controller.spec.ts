d5bb7e791c334f36533b0f8573d7493fa73cfcec
Silas Stoffel <silasstofel@gmail.com>
2023-03-13 13:15:21 -0300

    chore: setting supertest header

--- tests/e2e/modules/accounts/infra/http/load-account-controller.spec.ts
+++ tests/e2e/modules/accounts/infra/http/load-account-controller.spec.ts
@@ -1,5 +1,5 @@
 import { initializeDataSource } from "../../../../../helpers/setup-data-source";
-import request from 'supertest';
+import request, { agent as supertest } from 'supertest';
 import { app } from "../../../../../../src/shared/infra/http/app";
 
 describe("LoadAccountController", () => {
@@ -7,6 +7,7 @@ describe("LoadAccountController", () => {
     let bearer: string;
     const name = "Account 3";
     const id = "6bc53670-8747-4245-bc6f-458cf735f476";
+    const httpClient = supertest(app);
 
     beforeAll(async () => {
         await initializeDataSource();
@@ -18,18 +19,13 @@ describe("LoadAccountController", () => {
             password: "123456"
         }).expect(200);
 
-        bearer = `Bearer ${data.body.token}`;
+        httpClient.auth(data.body.token, { type: 'bearer' })
     });
 
     describe("when the credentials are correctly", () => {
         it('should return account info', async() => {
             console.log(bearer);
-            const response = await request(app).get('/auth/me')
-            .set("Authorization", bearer)
-            //.auth(bearer, { type: 'bearer' })
-            .expect(({ headers }) => {
-                console.log(headers);
-            });
+            const response = await httpClient.get('/auth/me');
 
             expect(response.status).toBe(200);
             expect(response.body).toHaveProperty('id', id);
