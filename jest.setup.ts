acdd9050f6ebf0b5166a7701c0efcd6f6d14647d
Silas Stoffel <silasstofel@gmail.com>
2023-03-13 12:15:30 -0300

    chore: adjusting setup for CI mode

--- jest.setup.ts
+++ jest.setup.ts
@@ -4,9 +4,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { AppDataSource } from "./src/shared/infra/database/typeorm/data-source";
-AppDataSource
-.initialize()
-.then(err => console.info('Database initialized.'))
-.catch(err => console.error('Connection failed. Detail: ', err.message));
+
+/*
+(async () => {
+    await AppDataSource.initialize();
+})();
+*/
+//AppDataSource
+//.initialize()
+//.then(err => console.info('Database initialized.'))
+//.catch(err => console.error('Connection failed. Detail: ', err.message));
 
 import "./src/shared/infra/providers/index";
